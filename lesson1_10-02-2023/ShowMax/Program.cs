2931a2efdffa4a432634940d694d2a01b984e7be
Yurii <iyureg@gmail.com>
2023-02-11 14:34:33 +0300

    add branch less1 + ShowMax Program

---
+++ lesson1_10-02-2023/ShowMax/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = int.Parse(Console.ReadLine()),
+    b = int.Parse(Console.ReadLine()),
+    c = int.Parse(Console.ReadLine()),
+    max = a;
+
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("Максимальное введенное число: " + max);
