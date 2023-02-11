5df908c9ad458c42d4d3a422f62015f23af1da08
Yurii <iyureg@gmail.com>
2023-02-11 15:04:00 +0300

    add Program ShowEvens

---
+++ lesson1_10-02-2023/ShowEvens/Program.cs
@@ -0,0 +1,12 @@
+﻿int randomNumber = new Random().Next(0, 101);
+
+
+for (int i = 0; i <= randomNumber; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine("Четное число: " + i);
+    }
+
+    
+}
