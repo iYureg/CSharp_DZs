d63949e0e14da1e2938869b99a418a26b73ef4c3
Yurii <iyureg@gmail.com>
2023-02-15 15:11:29 +0300

    ДЗ семинар 2 показать вторую цифру трехзначного числа

---
+++ lesson2_14-02-2023/ShowCenter/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа принимает на вход трехзначное число
+// и показывает вторую цифру этого числа
+
+
+
+int num = new Random().Next(100, 1000);
+
+int left = num / 100;
+int center = (num - (left * 100)) / 10;
+
+Console.WriteLine($"{num} ->  {center}");
+
