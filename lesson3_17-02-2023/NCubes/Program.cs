b9909b02729a09090bce66057e7d2fb8938dea66
Yurii <iyureg@gmail.com>
2023-02-18 22:53:44 +0300

    таблица кубов всех чисел от N

---
+++ lesson3_17-02-2023/NCubes/Program.cs
@@ -0,0 +1,39 @@
+﻿// Программа принимает на вход число N 
+// и выдает таблицу кубов чисел до N
+
+Console.Clear();
+
+int[] GetCubes(int num){
+
+    int i = 1, j = 0;
+    int length = num;
+
+    if(num < 0){
+
+        i = num;
+        length = -num;
+        length += 2;
+        num = 1;
+    }
+    int[] resultArray = new int[length];
+
+    for (; i <= num; i++){
+        resultArray[j] = Convert.ToInt32(Math.Pow(i, 3));
+        j++;
+    }
+    
+    return resultArray;
+}
+
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int[] result = GetCubes(N);
+
+for (int i = 0; i < result.Length; i++)
+{
+
+    Console.Write($"{result[i]} ");
+
+}
