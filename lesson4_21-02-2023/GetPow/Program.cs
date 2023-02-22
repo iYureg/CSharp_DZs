5629b3595f2623d1bc4f790ef474f4058fe8b402
Yurii <iyureg@gmail.com>
2023-02-22 14:22:55 +0300

    программа возводит число A в степень числа B

---
+++ lesson4_21-02-2023/GetPow/Program.cs
@@ -0,0 +1,22 @@
+﻿// написать цикл, который принимает на вход два числа (А и В)
+// и возводит число А в натуральную степень В
+
+double GetPower(int num, int pow){
+    double result = num;
+
+    for (int i = 1; i < pow; i++)
+    {
+        result *= num;
+    }
+
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите степень числа A: ");
+int power = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{number} в {power} степени = {GetPower(number, power)}");
+
