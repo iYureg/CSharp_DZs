fa24831d05e8026bfecb3ab6d8dedc618a273aa4
Yurii <iyureg@gmail.com>
2023-02-25 16:18:06 +0300

    сумма нечетных позиций массива

---
+++ lesson5_24-02-2023/SumOddPosition/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] GetRandomArray(int size){
+
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+
+    return array;
+}
+
+int GetOddSum(int[] arr){
+    int result = 0;
+
+    for (int i = 1; i < arr.Length; i += 2){
+        result += arr[i];
+    } 
+
+    return result;
+}
+
+int[] array = GetRandomArray(11);
+
+Console.Clear();
+Console.WriteLine($"[ {String.Join(", ", array)} ]");
+Console.WriteLine($"Сумма нечетных позиций = {GetOddSum(array)}");
