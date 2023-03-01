b00cd1c8036a7247fb772e86730d7880de40b4d3
Yurii <iyureg@gmail.com>
2023-03-01 16:51:39 +0300

    количество числел больше нуля

---
+++ lesson6_28-02-2023/CountPositive/Program.cs
@@ -0,0 +1,26 @@
+﻿// пользователь вводит произвольное количество чисел
+// программа считает сколько чисел больше нуля
+
+// 0, 7, 8, -2, -2  -> 2
+
+bool isPositive(int n){
+    return n > 0;
+}
+
+int CountPositive(string[] nums){
+    int counter = 0;
+    
+    for (int i = 0; i < nums.Length; i++)
+    {
+       counter += isPositive(int.Parse(nums[i])) ? 1 : 0;
+    }
+    return counter;
+}
+
+Console.Clear();
+Console.Write("Введите числа через пробел: ");
+string str = Console.ReadLine();
+
+string[] array = str.Split(" ");
+
+Console.WriteLine($"[ {String.Join(", ", array)} ]  вы ввели - {CountPositive(array)} - чисел больше нуля");
