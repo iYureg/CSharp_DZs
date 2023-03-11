7ed538beb60d91eebbbec461f1a1fbb32961f657
Yurii <iyureg@gmail.com>
2023-03-11 16:25:33 +0300

    сумма чисел от M до N через рекурсию

---
+++ lesson9_10-03-2023/SumNumsAfterMBeforeN/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите начало: ");
+int start = int.Parse(ReadLine());
+Write("Введите конец: ");
+int finish = int.Parse(ReadLine());
+
+WriteLine(ShowNumbers(start,finish));
+
+int ShowNumbers(int s, int f)
+{
+    if (s == f)
+    {
+        return f;
+    }
+
+    int sum = f < s 
+        ? ShowNumbers(s, f + 1) + f 
+        : ShowNumbers(s, f - 1) + f;
+    return sum;
+}
+
